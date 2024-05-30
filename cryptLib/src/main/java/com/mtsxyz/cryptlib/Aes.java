1225a5a264a1d9d4df7154d73f4504b086b6c200
MaoQiuxiang <373765724@qq.com>
2024-05-30 20:28:38 +0800

    初始化仓库

---
+++ cryptLib/src/main/java/com/mtsxyz/cryptlib/Aes.java
@@ -0,0 +1,12 @@
+package com.mtsxyz.cryptlib;
+/* ****************************************************
+ ** Function: Aes 加解密类
+ **
+ ** Author: MQX
+ ** Date: 2024-05-30
+ ** Copyright: 2024 www.mtsxyz.com All rights reserved.
+ ** ****************************************************/
+
+public class Aes {
+
+}
