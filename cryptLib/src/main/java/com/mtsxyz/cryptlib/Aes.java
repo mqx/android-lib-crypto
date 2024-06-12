155173f55c7fce51c89dd35be79a7654a47bbca7
Admin <14483671+mtsxyz_0@user.noreply.gitee.com>
2024-06-12 17:11:45 +0800

    完善Aes加解密及单元测试

--- cryptLib/src/main/java/com/mtsxyz/cryptlib/Aes.java
+++ cryptLib/src/main/java/com/mtsxyz/cryptlib/Aes.java
@@ -1,7 +1,29 @@
 package com.mtsxyz.cryptlib;
 /* ****************************************************
  ** Function: Aes 加解密类
- **
+ * https://developer.android.google.cn/privacy-and-security/cryptography?hl=el
+ ** 加密模式分为五种：
+ * ECB-电码本模式(Electronic Codebook Book)：
+ *      这种模式是将整个明文分成若干段相同的小段，然后对每一小段进行加密。
+ *      适合于单块小数据
+ * CBC-密码分组链接模式(Cipher Block Chaining):
+ *      这种模式是先将明文切分成若干小段，然后每一小段与初始块或者上一段的密文段进行异或运算后，
+ *      再与密钥进行加密。第一块就直接使用IV的值，其它块使用上一段的密文进行。
+ *      适合于大数据（强度比ECB大，分组通用）
+ * CTR-计算器模式(Counter)：
+ *      有一个自增的算子（计数器），这个算子用密钥加密之后的输出和明文异或的结果得到密文，相当于一次一密。
+ *      这种加密方式简单快速，安全可靠，而且可以并行加密，可以仅解密某数据块，计数值最好不能重复，否则将产生
+ *      相同的输出（用密钥加密计数器后的输出）。
+ *      适合于流式数据（快速计算）
+ * CFB-密码反馈模式（Cipher FeedBack）：
+ *      1，将初始向量（IV）与密钥通过加密算法生成第一轮的密文作为加密输出。
+ *      2，将第一轮的密文与明文异或，得到加密后的数据，同时将该加密后的数据作为下一轮的输入。
+ *      重复以上步骤，直到所有数据均加密完成。
+ *      适合于多通道（数据流通用）
+ * OFB-输出反馈模式（Output FeedBack）：
+ *      先用块加密器生成密钥流（Keystream），然后再将密钥流与明文流异或得到密文流，解密是先用块加密器生成密钥流，
+ *      再将密钥流与密文流异或得到明文，由于异或操作的对称性所以加密和解密的流程是完全一样的
+ *      适合于噪声信道流传输
  ** Author: MQX
  ** Date: 2024-05-30
  ** Copyright: 2024 www.mtsxyz.com All rights reserved.
@@ -12,38 +34,106 @@ import javax.crypto.spec.IvParameterSpec;
 import javax.crypto.spec.SecretKeySpec;
 
 public class Aes {
-    private String mAlgorithm = "AES/ECB/PKCS5Padding";
-    private byte[] mIv = new byte[16];
+    private static final String ALGORITHM = "AES";
+    private static final String DEF_PADDING = "AES/ECB/PKCS5Padding";
+    private static final int DEF_KEY_LEN = 32;
+    private static final int IV_LEN = 16;
+    private static final byte[] DEF_IV = new byte[IV_LEN];
+    // 填充模式
+    private String mPadding;
+    // 密钥长度（默认256位）
+    private int mKeyLength;
+    // 向量(偏移量)初始为全0
+    private byte[] mIv;
+    private byte[] mKey;
 
-    public void setAlgorithm(String algorithm){
-        mAlgorithm = algorithm;
+    public Aes() {
+        this(DEF_PADDING);
     }
-    public byte[] enc(byte[] data, byte[] key) throws Exception{
-        return doAes(true, data, key, 32, mIv, mAlgorithm);
+
+    public Aes(String padding) {
+        this(padding, DEF_KEY_LEN);
     }
 
-    public byte[] dec(byte[] data, byte[] key) throws Exception{
-        return doAes(false, data, key, 32, mIv, mAlgorithm);
+    public Aes(String padding, int keyLength) {
+        this(padding, keyLength, DEF_IV);
     }
 
-    private byte[] doAes(boolean isEnc, byte[] data, byte[] key, int keyLen, byte[] iv,
-                               String algorithm) throws Exception {
-        SecretKeySpec keySpec = new SecretKeySpec(key, 0, keyLen, algorithm);
+    public Aes(String padding, int keyLength, byte[] iv) {
+        this.mPadding = padding;
+        this.mKeyLength = keyLength;
+        this.mIv = iv;
+    }
 
-        Cipher cipher = Cipher.getInstance(algorithm);//Default "AES/ECB/PKCS5Padding"
-        if (mAlgorithm.contains("ECB")){
-            if (isEnc)
-                cipher.init(Cipher.ENCRYPT_MODE, keySpec);
-            else
-                cipher.init(Cipher.DECRYPT_MODE, keySpec);
-        }else{
-            IvParameterSpec ivParam = new IvParameterSpec(iv);
-            if (isEnc)
-                cipher.init(Cipher.ENCRYPT_MODE, keySpec, ivParam);
-            else
-                cipher.init(Cipher.DECRYPT_MODE, keySpec, ivParam);
+    public void setKey(byte[] key){
+        mKey = key;
+    }
+    public byte[] getKey(){
+        return mKey;
+    }
+
+    public void setPadding(String padding){
+        mPadding = padding;
+    }
+    public String getPadding(){
+        return mPadding;
+    }
+
+    public void setKeyLength(int keyLength){
+        mKeyLength = keyLength;
+    }
+    public int getKeyLength(){
+        return mKeyLength;
+    }
+
+    public void setIv(byte[] iv){
+        mIv = iv;
+    }
+    public byte[] getIv(){
+        return mIv;
+    }
+
+    public byte[] encrypt(byte[] data) throws Exception{
+        return encrypt(data, mKey);
+    }
+
+    public byte[] decrypt(byte[] data) throws Exception{
+        return decrypt(data, mKey);
+    }
+
+    public byte[] encrypt(byte[] data, byte[] key) throws Exception{
+        return doAes(Cipher.ENCRYPT_MODE, data, key, mKeyLength, mIv, mPadding);
+    }
+
+    public byte[] decrypt(byte[] data, byte[] key) throws Exception{
+        return doAes(Cipher.DECRYPT_MODE, data, key, mKeyLength, mIv, mPadding);
+    }
+
+    private byte[] doAes(int mode, byte[] data, byte[] key, int keyLen, byte[] iv,
+                         String padding) throws Exception {
+        // 如果加密的内容或密钥为null则直接返回（加/解密也会抛异常）
+        if (null == data || null == key)
+            return null;
+        byte[] useKey = key;
+        if (key.length != keyLen){
+            // 如果密钥长度和密钥内容长度不同则进行调整
+            useKey = new byte[keyLen];
+            System.arraycopy(key, 0, useKey, 0, Math.min(key.length, useKey.length));
         }
+        SecretKeySpec keySpec = new SecretKeySpec(useKey, 0, keyLen, ALGORITHM);
+        Cipher cipher = Cipher.getInstance(padding);
 
+        if (!padding.contains("ECB")) {
+            // 同样地，对偏移向量也要处理
+            byte[] useIv = iv;
+            if (iv.length != IV_LEN){
+                useIv = new byte[IV_LEN];
+                System.arraycopy(iv, 0, useIv, 0, Math.min(iv.length, useIv.length));
+            }
+            IvParameterSpec ivParam = new IvParameterSpec(useIv);
+            cipher.init(mode, keySpec, ivParam);
+        }else
+            cipher.init(mode, keySpec);
         return cipher.doFinal(data);
     }
 }
--- cryptLib/src/test/java/com/mtsxyz/cryptlib/ExampleUnitTest.java
+++ cryptLib/src/test/java/com/mtsxyz/cryptlib/ExampleUnitTest.java
@@ -3,6 +3,9 @@ package com.mtsxyz.cryptlib;
 import org.junit.Test;
 
 import static org.junit.Assert.*;
+import static java.lang.Math.min;
+
+import java.util.Arrays;
 
 /**
  * Example local unit test, which will execute on the development machine (host).
@@ -12,6 +15,160 @@ import static org.junit.Assert.*;
 public class ExampleUnitTest {
     @Test
     public void addition_isCorrect() {
-        assertEquals(4, 2 + 2);
+        try {
+            String test = "0123456789";
+            Aes aes = new Aes();
+            byte[] iv = new byte[16];
+            byte[] key = "key123456789".getBytes();
+//            test("AES/ECB/NoPadding", aes, test.getBytes(), key, 16);
+////            test("AES/ECB/ZeroPadding", aes, test.getBytes(), key, 16);
+//            test("AES/ECB/PKCS5Padding", aes, test.getBytes(), key, 16);
+////            test("AES/ECB/PKCS7Padding", aes, test.getBytes(), key, 16);
+//            test("AES/ECB/NoPadding", aes, test.getBytes(), key, 24);
+////            test("AES/ECB/ZeroPadding", aes, test.getBytes(), key, 24);
+//            test("AES/ECB/PKCS5Padding", aes, test.getBytes(), key, 24);
+////            test("AES/ECB/PKCS7Padding", aes, test.getBytes(), key, 24);
+//            test("AES/ECB/NoPadding", aes, test.getBytes(), key, 32);
+////            test("AES/ECB/ZeroPadding", aes, test.getBytes(), key, 32);
+//            test("AES/ECB/PKCS5Padding", aes, test.getBytes(), key, 32);
+////            test("AES/ECB/PKCS7Padding", aes, test.getBytes(), key, 32);
+//            // 以上被注释掉的是不支持的，NoPadding需要确保明文（字节）长度是16的整数倍
+
+//            test("AES/CBC/NoPadding", aes, test.getBytes(), key, 16, iv);
+//            test("AES/CBC/ZeroPadding", aes, test.getBytes(), key, 16, iv);
+//            test("AES/CBC/PKCS5Padding", aes, test.getBytes(), key, 16, iv);
+//            test("AES/CBC/PKCS7Padding", aes, test.getBytes(), key, 16, iv);
+//            test("AES/CBC/NoPadding", aes, test.getBytes(), key, 24, iv);
+//            test("AES/CBC/ZeroPadding", aes, test.getBytes(), key, 24, iv);
+//            test("AES/CBC/PKCS5Padding", aes, test.getBytes(), key, 24, iv);
+//            test("AES/CBC/PKCS7Padding", aes, test.getBytes(), key, 24, iv);
+//            test("AES/CBC/NoPadding", aes, test.getBytes(), key, 32, iv);
+//            test("AES/CBC/ZeroPadding", aes, test.getBytes(), key, 32, iv);
+//            test("AES/CBC/PKCS5Padding", aes, test.getBytes(), key, 32, iv);
+//            test("AES/CBC/PKCS7Padding", aes, test.getBytes(), key, 32, iv);
+            // 通过规则同ECB
+
+//            test("AES/CTR/NoPadding", aes, test.getBytes(), key, 16, iv);
+//            test("AES/CTR/ZeroPadding", aes, test.getBytes(), key, 16, iv);
+//            test("AES/CTR/PKCS5Padding", aes, test.getBytes(), key, 16, iv);
+//            test("AES/CTR/PKCS7Padding", aes, test.getBytes(), key, 16, iv);
+//            test("AES/CTR/NoPadding", aes, test.getBytes(), key, 24, iv);
+//            test("AES/CTR/ZeroPadding", aes, test.getBytes(), key, 24, iv);
+//            test("AES/CTR/PKCS5Padding", aes, test.getBytes(), key, 24, iv);
+//            test("AES/CTR/PKCS7Padding", aes, test.getBytes(), key, 24, iv);
+//            test("AES/CTR/NoPadding", aes, test.getBytes(), key, 32, iv);
+//            test("AES/CTR/ZeroPadding", aes, test.getBytes(), key, 32, iv);
+//            test("AES/CTR/PKCS5Padding", aes, test.getBytes(), key, 32, iv);
+//            test("AES/CTR/PKCS7Padding", aes, test.getBytes(), key, 32, iv);
+            // 该模式只有NoPadding能通过测试（无论明文是否16字节整数倍）
+
+//            test("AES/CFB/NoPadding", aes, test.getBytes(), key, 16, iv);
+//            test("AES/CFB/ZeroPadding", aes, test.getBytes(), key, 16, iv);
+//            test("AES/CFB/PKCS5Padding", aes, test.getBytes(), key, 16, iv);
+//            test("AES/CFB/PKCS7Padding", aes, test.getBytes(), key, 16, iv);
+//            test("AES/CFB/NoPadding", aes, test.getBytes(), key, 24, iv);
+//            test("AES/CFB/ZeroPadding", aes, test.getBytes(), key, 24, iv);
+//            test("AES/CFB/PKCS5Padding", aes, test.getBytes(), key, 24, iv);
+//            test("AES/CFB/PKCS7Padding", aes, test.getBytes(), key, 24, iv);
+//            test("AES/CFB/NoPadding", aes, test.getBytes(), key, 32, iv);
+//            test("AES/CFB/ZeroPadding", aes, test.getBytes(), key, 32, iv);
+//            test("AES/CFB/PKCS5Padding", aes, test.getBytes(), key, 32, iv);
+//            test("AES/CFB/PKCS7Padding", aes, test.getBytes(), key, 32, iv);
+            // 该模式只有NoPadding和PKCS5Padding能通过测试（无论明文是否16字节整数倍）
+
+            test("AES/OFB/NoPadding", aes, test.getBytes(), key, 16, iv);
+            test("AES/OFB/ZeroPadding", aes, test.getBytes(), key, 16, iv);
+            test("AES/OFB/PKCS5Padding", aes, test.getBytes(), key, 16, iv);
+            test("AES/OFB/PKCS7Padding", aes, test.getBytes(), key, 16, iv);
+            test("AES/OFB/NoPadding", aes, test.getBytes(), key, 24, iv);
+            test("AES/OFB/ZeroPadding", aes, test.getBytes(), key, 24, iv);
+            test("AES/OFB/PKCS5Padding", aes, test.getBytes(), key, 24, iv);
+            test("AES/OFB/PKCS7Padding", aes, test.getBytes(), key, 24, iv);
+            test("AES/OFB/NoPadding", aes, test.getBytes(), key, 32, iv);
+            test("AES/OFB/ZeroPadding", aes, test.getBytes(), key, 32, iv);
+            test("AES/OFB/PKCS5Padding", aes, test.getBytes(), key, 32, iv);
+            test("AES/OFB/PKCS7Padding", aes, test.getBytes(), key, 32, iv);
+            // 该模式通过规则同CFB
+
+
+            // 测试不包括密钥的参数
+            byte[] content = test.getBytes();
+            aes.setKey(key);
+            aes.setIv(iv);
+            aes.setKeyLength(16);
+            aes.setPadding("AES/OFB/NoPadding");
+            test(aes, content);
+            aes.setPadding("AES/OFB/ZeroPadding");
+            test(aes, content);
+            aes.setPadding("AES/OFB/PKCS5Padding");
+            test(aes, content);
+            aes.setPadding("AES/OFB/PKCS7Padding");
+            test(aes, content);
+            aes.setKeyLength(24);
+            aes.setPadding("AES/OFB/NoPadding");
+            test(aes, content);
+            aes.setPadding("AES/OFB/ZeroPadding");
+            test(aes, content);
+            aes.setPadding("AES/OFB/PKCS5Padding");
+            test(aes, content);
+            aes.setPadding("AES/OFB/PKCS7Padding");
+            test(aes, content);
+            aes.setKeyLength(32);
+            aes.setPadding("AES/OFB/NoPadding");
+            test(aes, content);
+            aes.setPadding("AES/OFB/ZeroPadding");
+            test(aes, content);
+            aes.setPadding("AES/OFB/PKCS5Padding");
+            test(aes, content);
+            aes.setPadding("AES/OFB/PKCS7Padding");
+            test(aes, content);
+            // 效果同上
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    private void test(String pkcs, Aes aes, byte[] testData, byte[] testKey, int keyLen){
+        aes.setKeyLength(keyLen);
+        byte[] k = new byte[keyLen];
+        System.arraycopy(testKey, 0, k, 0, min(k.length, testKey.length));
+        aes.setPadding(pkcs);
+        System.out.println("k-len:"+keyLen+", padding:"+pkcs);
+        try {
+            byte[] encData = aes.encrypt(testData, k);
+            byte[] decData = aes.decrypt(encData, k);
+            assertArrayEquals(testData, decData);
+            System.out.println("Pass");
+        } catch (Exception e) {
+            System.out.println("Err");
+        }
+    }
+
+    private void test(String pkcs, Aes aes, byte[] testData, byte[] testKey, int keyLen, byte[] iv){
+        aes.setKeyLength(keyLen);
+        aes.setIv(iv);
+        byte[] k = new byte[keyLen];
+        System.arraycopy(testKey, 0, k, 0, min(k.length, testKey.length));
+        aes.setPadding(pkcs);
+        System.out.println("k-len:"+keyLen+", padding:"+pkcs);
+        try {
+            byte[] encData = aes.encrypt(testData, k);
+            byte[] decData = aes.decrypt(encData, k);
+            assertArrayEquals(testData, decData);
+            System.out.println("Pass");
+        } catch (Exception e) {
+            System.out.println("Err");
+        }
+    }
+
+    private void test(Aes aes, byte[] testData){
+        try {
+            byte[] encData = aes.encrypt(testData);
+            byte[] decData = aes.decrypt(encData);
+            assertArrayEquals(testData, decData);
+            System.out.println("Pass");
+        } catch (Exception e) {
+            System.out.println("Err");
+        }
     }
 }
